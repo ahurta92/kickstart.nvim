39e075c26abd044ecbefc2cb1f9cd2e8eea1ca63
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2025-08-13 10:25:29 -0400

    Remove latex and latexindent from tree-sitter

--- init.lua
+++ init.lua
@@ -774,7 +774,6 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         json = { 'prettier' },
-        ['tex'] = { 'latexindent' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
         --
@@ -953,7 +952,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'latex' },
+      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
 
       -- Autoinstall languages that are not installed
       auto_install = true,
